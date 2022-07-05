f3dff8a7c6c6213270bb2ee9ebbebc33ea6170f1
himesha98 <himeshmahee99@gmail.com>
2022-07-05 22:04:56 +0530

    first upoad

---
+++ models/Conversation.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const ConversationSchema = new mongoose.Schema(
+    {
+        members:{
+            type:Array,
+        }
+
+    },
+
+    { timestamps: true }
+
+);
+
+module.exports = mongoose.model("Conversation", ConversationSchema);
+
+
